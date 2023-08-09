1b519c2f8cc35ad339d8edd698f0f910efec0d96
frankmilito <frankobidike20@gmail.com>
2023-08-09 09:31:16 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import {} from "react";
+
+import "./App.css";
+import Sidemenu from "./components/Sidemenu";
+
+function App() {
+  return (
+    <div className="flex">
+      <Sidemenu />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidemenu.jsx
@@ -0,0 +1,5 @@
+const Sidemenu = () => {
+  return <div className="flex-none bg-red-200 w-36 h-screen">d</div>;
+};
+
+export default Sidemenu;
